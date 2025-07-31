cb2b95500a72257322d78829c2475e6388bd8433
Kshitij-git2006 <it24.kshitij.parkhe@kccemsr.edu.in>
2025-07-31 23:12:59 +0530

    Add files via upload

---
+++ diseaseapi/diseaseapi/src/test/java/com/disease/diseaseapi/DiseaseapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disease.diseaseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiseaseapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
